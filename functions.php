2e99b88d477bbecc19493d9e111b26873556f677
Prachi1510 <46515238+Prachi1510@users.noreply.github.com>
2019-01-15 15:08:28 +1000

    Update functions.php
    Secondary menu didnt work so changed it to primary

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'jcu_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Secondary', 'jcu' ),
+			'menu-1' => esc_html__( 'Primary', 'jcu' ),
 		) );
 
 		/*
