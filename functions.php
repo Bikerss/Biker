50e3c6ad4224b7da975376999e9b127f33b825e9
gursheen123 <45142933+gursheen123@users.noreply.github.com>
2019-01-15 15:04:53 +1000

    Update functions.php
    changes some specifications as not fitted according to page

--- functions.php
+++ functions.php
@@ -74,8 +74,8 @@ if ( ! function_exists( 'jcu_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
+			'height'      => 350,
+			'width'       => 350,
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
