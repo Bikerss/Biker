70504e937243aee153fc6824fa0bb0e8cdb303f1
Prachi1510 <46515238+Prachi1510@users.noreply.github.com>
2019-01-15 15:07:20 +1000

    Update functions.php
    I have changed the menu as secondary menu

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'jcu_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'jcu' ),
+			'menu-1' => esc_html__( 'Secondary', 'jcu' ),
 		) );
 
 		/*
@@ -74,8 +74,8 @@ if ( ! function_exists( 'jcu_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'      => 350,
-			'width'       => 350,
+			'height'      => 250,
+			'width'       => 250,
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
